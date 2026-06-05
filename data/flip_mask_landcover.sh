1272aab1f66f17b517bfb2db524283e3a067e564
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2026-06-05 11:40:14 +0200

    Create flip_mask_landcover.sh

---
+++ data/flip_mask_landcover.sh
@@ -0,0 +1,11 @@
+python flip_mask_landcover.py --var natural --biof '' --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var cropland --biof '' --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var pasture --biof '' --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+
+python flip_mask_landcover.py --var natural --biof '' --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var cropland --biof '' --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var pasture --biof '' --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python flip_mask_landcover.py --var natural --biof '' --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var cropland --biof '' --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+python flip_mask_landcover.py --var pasture --biof '' --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
