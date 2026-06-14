397e3c652714237b56338254f9d4cd0b43034bb4
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2026-06-14 15:47:37 +0200

    Create calc_dry_days.sh

---
+++ data/calc_dry_days.sh
@@ -0,0 +1,11 @@
+python calc_dry_days.py --exp_id a7cy --realisation r1i1p1f1
+python calc_dry_days.py --exp_id a7cy --realisation r2i1p1f1
+python calc_dry_days.py --exp_id a7cy --realisation r3i1p1f1
+
+python calc_dry_days.py --exp_id a7en --realisation r1i1p1f1
+python calc_dry_days.py --exp_id a7en --realisation r4i1p1f1
+python calc_dry_days.py --exp_id a7en --realisation r3i1p1f1
+
+python calc_dry_days.py --exp_id a7eo --realisation r1i1p1f1
+python calc_dry_days.py --exp_id a7eo --realisation r2i1p1f1
+python calc_dry_days.py --exp_id a7eo --realisation r3i1p1f1
