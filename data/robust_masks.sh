3759f11b9979cada430687f56527eb9962fda4e3
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2026-06-14 15:54:49 +0200

    Update robust_masks.sh

--- data/robust_masks.sh
+++ data/robust_masks.sh
@@ -1,3 +1,4 @@
+### Main
 python robust_masks.py --var albedo --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
 python robust_masks.py --var albedo --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
 python robust_masks.py --var albedo --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
@@ -9,3 +10,48 @@ python robust_masks.py --var hfls --exp_ref a7en --exp_pert a7eo --first_year 20
 python robust_masks.py --var cLand --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
 python robust_masks.py --var cLand --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
 python robust_masks.py --var cLand --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+### Supplement
+python robust_masks.py --var albedo --exp_ref a7en --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var albedo --exp_ref a7eo --exp_pert a7eo --first_year 2071 --last_year 2100
+python robust_masks.py --var albedo --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var hfls --exp_ref a7en --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var hfls --exp_ref a7eo --exp_pert a7eo --first_year 2071 --last_year 2100
+python robust_masks.py --var hfls --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cLand --exp_ref a7en --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cLand --exp_ref a7eo --exp_pert a7eo --first_year 2071 --last_year 2100
+python robust_masks.py --var cLand --exp_ref a7cy --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cVeg --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var cVeg --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cVeg --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cSoil --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var cSoil --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cSoil --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cLitter --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var cLitter --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cLiter --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cBECCS --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var cBECCS --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cBECCS --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var cBiochar --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var cBiochar --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var cBiochar --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var fco2fossub --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var fco2fossub --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var fco2fossub --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var tas --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var tas --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var tas --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
+
+python robust_masks.py --var pr --exp_ref a7cy --exp_pert a7cy --first_year 2071 --last_year 2100
+python robust_masks.py --var pr --exp_ref a7cy --exp_pert a7en --first_year 2071 --last_year 2100
+python robust_masks.py --var pr --exp_ref a7en --exp_pert a7eo --first_year 2071 --last_year 2100
