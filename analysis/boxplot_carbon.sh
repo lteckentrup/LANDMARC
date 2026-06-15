bad64ce3fe7b85606c68bb943fa2eff10e84ad0f
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2026-06-15 17:10:05 +0200

    Create boxplot_carbon.sh

---
+++ analysis/boxplot_carbon.sh
@@ -0,0 +1,4 @@
+python boxplot_carbon.py --region Global --first_year 2071 --last_year 2100
+python boxplot_carbon.py --region Tropical --first_year 2071 --last_year 2100
+python boxplot_carbon.py --region Temperate --first_year 2071 --last_year 2100
+python boxplot_carbon.py --region Boreal --first_year 2071 --last_year 2100
