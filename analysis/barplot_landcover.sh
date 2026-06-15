7157a639e0943aa85cb8b12ae1bf1441dc0f0810
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2026-06-15 17:09:31 +0200

    Create barplot_landcover.sh

---
+++ analysis/barplot_landcover.sh
@@ -0,0 +1,4 @@
+python barplot_landcover.py --region Global
+python barplot_landcover.py --region Tropical
+python barplot_landcover.py --region Temperate
+python barplot_landcover.py --region Boreal
